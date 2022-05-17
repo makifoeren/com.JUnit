de8ae2d32bc8405204c8c8d54266c614942e5598
makifoeren <makif.oeren@gmail.com>
2022-05-17 09:44:02 +0200

    17.05

---
+++ src/test/java/day07_assertions/C02_BestBuy.java
@@ -0,0 +1,15 @@
+package day07_assertions;
+
+public class C02_BestBuy {
+
+
+    // 2) https://www.bestbuy.com/ Adresine gidin
+    //    farkli test method’lari olusturarak asagidaki testleri yapin
+    //      ○ Sayfa URL’inin https://www.bestbuy.com/‘a esit oldugunu test edin
+    //      ○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+    //      ○ logoTest => BestBuy logosunun görüntülendigini test edin
+    //      ○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
+
+
+
+}
---
+++ src/test/java/day07_assertions/Tanim.java
@@ -0,0 +1,29 @@
+package day07_assertions;
+
+public class Tanim {
+
+    /*
+    Assertions
+    Assertions methodlari
+    assertTrue,assertFalse de sonucu bilmemiz gerekiyor onmli olan  bekledigimiz sonucun olmamais.
+    Assertion basarisiz olursa ASSERTIONEXCEPTION hatasi olur.ne istioyrsak ona gore secip yazmaliyz
+
+    String exceptedName"Ali Can";
+    String actualName"Ali Can";
+    actualName.equals(exceptedName)---> false
+    actualName.equalsIgnoreCase(exceptedName)-->true
+
+    int yas1=63;
+    int yas2=68;
+    emekliOlabilirmi(yas1);-->false
+    emekliOlabilirmi(yas2);-->true
+
+Assert.assertEquqls(actualName,exceptedName) --false---TEST FAILED
+Assert.assertTrue(yas2>65)--true---PASSED
+Assert.assertTrue(yas2<65)--false---FAILED
+Assert.assertFalse(yas1>65)--false---PASSED
+Assert.assertFalse(yas1<65)--true---FAILED
+
+
+     */
+}
