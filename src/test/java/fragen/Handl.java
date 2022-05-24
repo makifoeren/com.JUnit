8c1720912b9c63050c5e4f84b7539e283822d5c2
makifoeren <makif.oeren@gmail.com>
2022-05-24 11:15:25 +0200

    24.05

--- src/test/java/fragen/Handl.java
+++ src/test/java/fragen/Handl.java
@@ -1,19 +1,102 @@
 package fragen;
 
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
+import java.util.Set;
+
 public class Handl extends TestBase {
-      /*
-    Window Handle HomeWork
-1."http://webdriveruniversity.com/" adresine gidin
-2."Login Portal" a kadar asagi inin
-3."Login Portal" a tiklayin
-4.Diger window'a gecin
-5."username" ve "password" kutularina deger yazdirin
-6."login" butonuna basin
-7.Popup'ta cikan yazinin "validation failed" oldugunu test edin
-8.Ok diyerek Popup'i kapatin
-9.Ilk sayfaya geri donun
-10.Ilk sayfaya donuldugunu test edin
-     */
+
+
+
+    @Test
+    public void test() throws InterruptedException {
+
+        //    Window Handle HomeWork
+        //1."http://webdriveruniversity.com/" adresine gidin
+
+        driver.get("http://webdriveruniversity.com/");
+
+        String ilkSayfaUrl=driver.getCurrentUrl();
+        System.out.println("ilkSayfaUrl = " + ilkSayfaUrl);
+
+        String ilkSayfaHandleDegeri = driver.getWindowHandle();
+        System.out.println("ilkSayfaHandleDegeri = " + ilkSayfaHandleDegeri);
+
+
+        //2."Login Portal" a kadar asagi inin
+        //3."Login Portal" a tiklayin
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//*[text()='LOGIN PORTAL']")).click();
+
+
+        //String sonSayfaHandleDegeri = driver.getWindowHandle();
+        //System.out.println("sonSayfaHandleDegeri = " + sonSayfaHandleDegeri);
+
+        //4.Diger window'a gecin
+
+        Set<String> tumHandles = driver.getWindowHandles();
+        System.out.println("tumHandles = " + tumHandles);
+
+        String ikinciSayfaHand = "";
+        for (String each : tumHandles
+        ) {
+            if (!each.equals(ilkSayfaHandleDegeri));
+            ikinciSayfaHand=each;
+
+        }
+
+        System.out.println("ikinciSayfaHand = " + ikinciSayfaHand);
+
+        //ikinci sayfaya gidelim
+        driver.switchTo().window(ikinciSayfaHand);
+
+
+        //5."username" ve "password" kutularina deger yazdirin
+        //6."login" butonuna basin
+
+        Faker faker=new Faker();
+        WebElement username=driver.findElement(By.xpath("//input[@placeholder='Username']"));
+
+        actions.click(username).
+                sendKeys(faker.name().username()).
+                sendKeys(Keys.TAB).
+                sendKeys(faker.internet().password()).
+                sendKeys(Keys.TAB).
+               sendKeys(Keys.ENTER).perform();
+
+        //7.Popup'ta cikan yazinin "validation failed" oldugunu test edin
+
+        String expectedPopup="validation failed";
+        String actualPopup=driver.switchTo().alert().getText();
+
+        Assert.assertEquals(expectedPopup,actualPopup);
+
+
+        //8.Ok diyerek Popup'i kapatin
+        driver.switchTo().alert().accept();
+
+        //9.Ilk sayfaya geri donun
+        driver.switchTo().window(ilkSayfaHandleDegeri);
+        Thread.sleep(2000);
+
+        //10.Ilk sayfaya donuldugunu test edin
+       String sonsayfaUrl= driver.getCurrentUrl();
+
+       Assert.assertEquals(ilkSayfaUrl,sonsayfaUrl);
+
+
+
+    }
 }
--- src/test/java/fragen/Iframe.java
+++ src/test/java/fragen/Iframe.java
@@ -7,17 +7,7 @@ import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
 public class Iframe extends TestBase {
-        /*
-   Iframe Home Work
-1. “http://webdriveruniversity.com/IFrame/index.html” sayfasina gidin
-2. “Our Products” butonuna basin
-3. “Cameras product”i tiklayin
-4. Popup mesajini yazdirin
 
-5. “close” butonuna basin
-6. "WebdriverUniversity.com (IFrame)" linkini tiklayin
-7. "http://webdriveruniversity.com/index.html" adresine gittigini test edin
-    */
 
     @Test
     public void test() throws InterruptedException {
@@ -27,30 +17,35 @@ public class Iframe extends TestBase {
         //2. “Our Products” butonuna basin
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@src='../Page-Object-Model/index.html']")));
         driver.findElement(By.xpath("//*[text()='Our Products']")).click();
+        Thread.sleep(2000);
 
         //3. “Cameras product”i tiklayin
         driver.findElement(By.xpath("//*[text()='Cameras']")).click();
         Thread.sleep(2000);
 
         //4. Popup mesajini yazdirin
-        WebElement popupMsj=driver.findElement(By.xpath("//div[@class='modal-body']"));
-        String popupYazi= popupMsj.getText();
-        System.out.println("popupYazi = " + popupYazi);
-
-        Thread.sleep(2000);
+        WebElement popunMsj=driver.findElement(By.xpath("//div[@class='modal-body']"));
+        String popunMsjYazi=popunMsj.getText();
+        System.out.println("popunMsjYazi = " + popunMsjYazi);
 
         //5. “close” butonuna basin
         driver.findElement(By.xpath("//*[text()='Close']")).click();
-        Thread.sleep(2000);
+
 
         //6. "WebdriverUniversity.com (IFrame)" linkini tiklayin
+
+        //driver.switchTo( ).parentFrame( ); 1 ust seviyedeki frame’e cikartir
         driver.switchTo().defaultContent(); //En ustteki frame’e cikmak icin kullanilir
+
         driver.findElement(By.xpath("(//a[@id='nav-title'])[1]")).click();
 
+
         //7. "http://webdriveruniversity.com/index.html" adresine gittigini test edin
+
         String expectedUrl="http://webdriveruniversity.com/index.html";
         String actualUrl=driver.getCurrentUrl();
-        Assert.assertTrue(expectedUrl.contains(actualUrl));
+
+        Assert.assertEquals(expectedUrl,actualUrl);
 
 
     }
